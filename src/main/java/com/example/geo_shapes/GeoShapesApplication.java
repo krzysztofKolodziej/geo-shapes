0642d52e801c52a3d9a8cb76b50acd6875d0a458
Krzysztof Kolodziej <krzysztof.kolodziej2212@gmail.com>
2025-04-14 18:43:14 +0200

    Initial commit

---
+++ src/main/java/com/example/geo_shapes/GeoShapesApplication.java
@@ -0,0 +1,13 @@
+package com.example.geo_shapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeoShapesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeoShapesApplication.class, args);
+	}
+
+}
